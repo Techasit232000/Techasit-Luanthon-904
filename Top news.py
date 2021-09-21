bf34c998ccdf2b33c6d6ef9e9ab045548fd31050
เตชสิทธ์ ล้วนทร <79820254+Techasit232000@users.noreply.github.com>
2021-09-21 16:22:22 +0700

    Add files via upload

---
+++ Top news.py
@@ -0,0 +1,7 @@
+Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 08:53:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("Top News is The fack News ofThailand")
+Top News is The fack News ofThailand
+>>> print (" Fack News https://www.topnews.co.th/")
+ Fack News https://www.topnews.co.th/
+>>> 
