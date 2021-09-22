550353e0725fb70675cfaceeaf01026498c09cfa
เตชสิทธ์ ล้วนทร <79820254+Techasit232000@users.noreply.github.com>
2021-09-23 05:57:01 +0700

    Add files via upload

---
+++ Srirat Suwadee.py
@@ -0,0 +1,9 @@
+Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 08:53:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("# Auto detect text files and perform LF normalization
+* text=auto/Srirat Suwadee")
+       
+SyntaxError: EOL while scanning string literal
+>>> print ("Edit to Srirat Suwadee")
+Edit to Srirat Suwadee
+>>> 
