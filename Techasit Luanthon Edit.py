a3ebae5a600bf6eea774df1bbca9ac8479f37c8a
เตชสิทธ์ ล้วนทร <79820254+Techasit232000@users.noreply.github.com>
2021-09-22 07:13:36 +0700

    Add files via upload

---
+++ Techasit Luanthon Edit.py
@@ -0,0 +1,22 @@
+Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 08:53:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("<!doctype html><title>Blockchain.com Wallet - Exchange Cryptocurrency</title><script nonce="zbzet6JnGJgxFcONTbsgc0No0qm7pIaR">window.NONCE = 'zbzet6JnGJgxFcONTbsgc0No0qm7pIaR'</script><style nonce="zbzet6JnGJgxFcONTbsgc0No0qm7pIaR">html, body, #app, #app > div {padding: 0; margin: 0; height: 100%;} html, body {overflow: hidden;} /* hide scrollbars */ ::-webkit-scrollbar { display: none; } * { scrollbar-width: none; -ms-overflow-style: none; -webkit-font-smoothing: antialiased; }</style>
+
+<script src="/manifest.1621360505456.js"></script><script src="/vendor.96b947fc3c.js"></script><script src="/frontend.d60e8a105d.js"></script><script src="/app.9587fd0f51.js"></script>")
+       
+SyntaxError: invalid syntax
+>>> print ("<!doctype html><title>Blockchain.com Wallet - Exchange Cryptocurrency</title><script nonce=" Techasit LuanthonR">window.NONCE = 'zbzet6JnGJgxFcONTbsgc0No0qm7pIaR'</script><style nonce="zbzet6JnGJgxFcONTbsgc0No0qm7pIaR">html, body, #app, #app > div {padding: 0; margin: 0; height: 100%;} html, body {overflow: hidden;} /* hide scrollbars */ ::-webkit-scrollbar { display: none; } * { scrollbar-width: none; -ms-overflow-style: none; -webkit-font-smoothing: antialiased; }</style>
+
+<script src="/manifest.1621360505456.js"></script><script src="/vendor.96b947fc3c.js"></script><script src="/frontend.d60e8a105d.js"></script><script src="/app.9587fd0f51.js"></script>")
+       
+SyntaxError: invalid syntax
+>>> print ("<!doctype html><title>Blockchain.com Wallet - Exchange Cryptocurrency</title><script nonce="R">window.NONCE = 'zbzet6JnGJgxFcONTbsgc0No0qm7pIaR'</script><style nonce="zbzet6JnGJgxFcONTbsgc0No0qm7pIaR">html, body, #app, #app > div {padding: 0; margin: 0; height: 100%;} html, body {overflow: hidden;} /* hide scrollbars */ ::-webkit-scrollbar { display: none; } * { scrollbar-width: none; -ms-overflow-style: none; -webkit-font-smoothing: antialiased; }</style>
+
+<script src="/manifest.1621360505456.js"></script><script src="/vendor.96b947fc3c.js"></script><script src="/frontend.d60e8a105d.js"></script><script src="/app.9587fd0f51.js"></script>")
+       
+SyntaxError: invalid syntax
+>>> print ("Techasit Luanthon")
+Techasit Luanthon
+>>> print ("zbzet6JnGJgxFcONTbsgc0No0qm7pm7pIar Edit to Techasit Luanthon")
+zbzet6JnGJgxFcONTbsgc0No0qm7pm7pIar Edit to Techasit Luanthon
+>>> 
