800e7236b23d702a6b532e68d455de5b1d0ab836
เตชสิทธ์ ล้วนทร <79820254+Techasit232000@users.noreply.github.com>
2021-09-22 12:29:13 +0700

    Add files via upload

---
+++ Server Error.py
@@ -0,0 +1,29 @@
+Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 08:53:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> PRINT('https://www.iqnewscenter.com/Cauth/login.aspx")
+      
+SyntaxError: EOL while scanning string literal
+>>> Print ("https://www.iqnewscenter.com/Cauth/login.aspx/Username Techasit14607/Password @Techasit1198837")
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    Print ("https://www.iqnewscenter.com/Cauth/login.aspx/Username Techasit14607/Password @Techasit1198837")
+NameError: name 'Print' is not defined
+>>> print ("/<!-- Web.Config Configuration File -->
+
+<configuration>
+    <system.web>
+        <customErrors mode="Off"/>
+    </system.web>
+</configuration>")
+       
+SyntaxError: EOL while scanning string literal
+>>> print ("<!-- Web.Config Configuration File -->
+
+<configuration>
+    <system.web>
+        <customErrors mode="RemoteOnly" defaultRedirect="mycustompage.htm"/>
+    </system.web>
+</configuration>")
+       
+SyntaxError: EOL while scanning string literal
+>>> 
