0aceed67db69a633bc9fdbda0779708760e781e5
เตชสิทธ์ ล้วนทร <79820254+Techasit232000@users.noreply.github.com>
2021-09-28 10:54:56 +0700

    Add files via upload

---
+++ Srirat Suwadee is the queen.py
@@ -0,0 +1,32 @@
+Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 08:53:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("https://github.com/Techasit232000/srirasmi914.git")
+https://github.com/Techasit232000/srirasmi914.git
+>>> print ("<ASCII/Latin1>
+AaBbCcDdEeFfGgHhIiJj
+1234567890#:+=(){}[]
+¢£¥§©«®¶½ĞÀÁÂÃÄÅÇÐØß
+
+<IPA,Greek,Cyrillic>
+ɐɕɘɞɟɤɫɮɰɷɻʁʃʆʎʞʢʫʭʯ
+ΑαΒβΓγΔδΕεΖζΗηΘθΙιΚκ
+БбДдЖжПпФфЧчЪъЭэѠѤѬӜ
+
+<Hebrew, Arabic>
+אבגדהוזחטיךכלםמןנסעף
+ابجدهوزحطي٠١٢٣٤٥٦٧٨٩
+
+<Devanagari, Tamil>
+०१२३४५६७८९अआइईउऊएऐओऔ
+௦௧௨௩௪௫௬௭௮௯அஇஉஎ
+
+<East Asian>
+〇一二三四五六七八九
+汉字漢字人木火土金水
+가냐더려모뵤수유즈치
+あいうえおアイウエオ")
+       
+SyntaxError: EOL while scanning string literal
+>>> print ("Srirat Suwadee is the queen")
+Srirat Suwadee is the queen
+>>> 
