2606560b7cfd289c65bbdb5589a66ecf6881e028
เตชสิทธ์ ล้วนทร <79820254+Techasit232000@users.noreply.github.com>
2021-09-28 10:50:11 +0700

    Add files via upload

---
+++ Prayut Chan-O-ChaGet out of The prime minister of Thailand.py
@@ -0,0 +1,5 @@
+Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 08:53:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("Prayut Chan-O-Cha" "Get out of The prime minister of Thailand")
+Prayut Chan-O-ChaGet out of The prime minister of Thailand
+>>> 
