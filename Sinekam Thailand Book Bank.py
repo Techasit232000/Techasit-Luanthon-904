589f38bd866b3ebf0d26435b619925842fce48bd
เตชสิทธ์ ล้วนทร <79820254+Techasit232000@users.noreply.github.com>
2021-09-30 05:05:55 +0700

    Add files via upload

---
+++ Sinekam Thailand Book Bank.py
@@ -0,0 +1,5 @@
+Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 08:53:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("Bangkok bank Sinekam Thailand 4520430242 Contact Techasit Luanthon Tel. +66953150050 or Raviwan Suwansamut  +66826346500")
+Bangkok bank Sinekam Thailand 4520430242 Contact Techasit Luanthon Tel. +66953150050 or Raviwan Suwansamut  +66826346500
+>>> 
