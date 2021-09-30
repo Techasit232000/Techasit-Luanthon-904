77efb5da1868fc41b35088c1a5dd3dc55e09749c
เตชสิทธ์ ล้วนทร <79820254+Techasit232000@users.noreply.github.com>
2021-10-01 05:34:52 +0700

    Add files via upload

---
+++ เป๋าตัง.py
@@ -0,0 +1,14 @@
+Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 08:53:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("เป๋าตัง โอนเงินเข้าบัญชีธนาคารได้")
+เป๋าตัง โอนเงินเข้าบัญชีธนาคารได้
+>>> ("บัญชีธนาคารSCB 4180211232 Techasit Luanthon")
+'บัญชีธนาคารSCB 4180211232 Techasit Luanthon'
+>>> Print ("Mobile number +66953150050")
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    Print ("Mobile number +66953150050")
+NameError: name 'Print' is not defined
+>>> print ("Edit o๙๕๓๑๕oo๕o")
+Edit o๙๕๓๑๕oo๕o
+>>> 
