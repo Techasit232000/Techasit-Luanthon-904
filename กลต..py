2e622f5a9ce7baa8d9eb3a27ae5aeeffd1fd4852
เตชสิทธ์ ล้วนทร <79820254+Techasit232000@users.noreply.github.com>
2021-10-04 05:51:30 +0700

    Add files via upload

---
+++ กลต..py
@@ -0,0 +1,9 @@
+Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 08:53:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("GitHub Desktop")
+GitHub Desktop
+>>> print ("/Thailand ban BTC and other online cryptocurrency")
+/Thailand ban BTC and other online cryptocurrency
+>>> print ("กลต. ไม่อนุมัติของการมีอยู่ของสกุลงินดิจิตอล")
+กลต. ไม่อนุมัติของการมีอยู่ของสกุลงินดิจิตอล
+>>> 
