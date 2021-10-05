d037805cbe169a937b00783b147a5db16a16624c
เตชสิทธ์ ล้วนทร <79820254+Techasit232000@users.noreply.github.com>
2021-10-05 11:41:12 +0700

    Add files via upload

---
+++ INTO THE BORDERLAND.py
@@ -0,0 +1,7 @@
+Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 08:53:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("<title>INTO THE BORDERLAND | หุ้นแนะนำเปิดเมืองวันนี้แนะนำกลุ่มไมเนอร์ค่ะ</title>")
+<title>INTO THE BORDERLAND | หุ้นแนะนำเปิดเมืองวันนี้แนะนำกลุ่มไมเนอร์ค่ะ</title>
+>>> print ("เตชสิทธ์ ล้วนทร")
+เตชสิทธ์ ล้วนทร
+>>> 
