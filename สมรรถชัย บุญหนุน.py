41dd4a0573e4a906d7546a09a40b135ff6371054
เตชสิทธ์ ล้วนทร <79820254+Techasit232000@users.noreply.github.com>
2021-10-05 19:10:06 +0700

    Add files via upload

---
+++ สมรรถชัย บุญหนุน.py
@@ -0,0 +1,10 @@
+Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 08:53:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> Print ("ค่ายมวยไชยาสมรรถชัย ที่อยู่ หมู่บ้านสี่ไชยทอง3 ซอย22 บ้านเลขที่25/472 ตำบลปากเกร็ด อำเภอปากเกร็ด จังหวัดนนทบุรี รหัสไปรษณีย์11120")
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    Print ("ค่ายมวยไชยาสมรรถชัย ที่อยู่ หมู่บ้านสี่ไชยทอง3 ซอย22 บ้านเลขที่25/472 ตำบลปากเกร็ด อำเภอปากเกร็ด จังหวัดนนทบุรี รหัสไปรษณีย์11120")
+NameError: name 'Print' is not defined
+>>> print ("สมรรถชัย บุญหนุน")
+สมรรถชัย บุญหนุน
+>>> 
