688a8e0c49d2da3fb4afdb5ef1cc9398bec30635
เตชสิทธ์ ล้วนทร <79820254+Techasit232000@users.noreply.github.com>
2021-10-08 07:14:40 +0700

    Add files via upload

---
+++ Pulling.py
@@ -0,0 +1,26 @@
+Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 08:53:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("warning: Pulling without specifying how to reconcile divergent branches is
+discouraged. You can squelch this message by running one of the following
+commands sometime before your next pull:
+
+  git config pull.rebase false  # merge (the default strategy)
+  git config pull.rebase true   # rebase
+  git config pull.ff only       # fast-forward only
+
+You can replace "git config" with "git config --global" to set a default
+preference for all repositories. You can also pass --rebase, --no-rebase,
+or --ff-only on the command line to override the configured default per
+invocation.
+
+remote: Enumerating objects: 31, done.        
+remote: Counting objects: 100% (31/31), done.        
+remote: Compressing objects: 100% (30/30), done.        
+remote: Total 30 (delta 12), reused 0 (delta 0), pack-reused 0        
+From https://github.com/Techasit232000/Techasit-Luanthon-904
+   f3a9119..ac35202  main       -> origin/main
+error: invalid path 'https:/www.mdes.go.th/member/re-send'
+Updating df7711d..ac35202")
+       
+SyntaxError: EOL while scanning string literal
+>>> 
