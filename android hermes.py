f63e94c270bd1f7523e6ab3b0743e49c4139002e
เตชสิทธ์ ล้วนทร <79820254+Techasit232000@users.noreply.github.com>
2021-10-11 09:33:53 +0700

    Add files via upload

---
+++ android hermes.py
@@ -0,0 +1,14 @@
+Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 08:53:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Platform	Engine	Arch	Size (bytes)	Diff
+android	hermes	arm64-v8a	7,717,940	+0
+android	hermes	armeabi-v7a	7,246,007	+0
+android	hermes	x86	8,137,533	+0
+android	hermes	x86_64	8,103,013	+0
+android	jsc	arm64-v8a	9,637,130	+0
+android	jsc	armeabi-v7a	8,552,720	+0
+android	jsc	x86	9,650,995	+0
+android	jsc	x86_64	10,260,163	+0")
+      
+SyntaxError: EOL while scanning string literal
+>>> 
