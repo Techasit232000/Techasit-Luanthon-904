da6454a85fb325de8fac229e6ebe0b2e498038b3
เตชสิทธ์ ล้วนทร <79820254+Techasit232000@users.noreply.github.com>
2021-10-17 20:28:32 +0700

    Add files via upload

---
+++ สมบัติ พูลสวัด รักกันดีกับ กรกต ล้วนทร.py
@@ -0,0 +1,4 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print ("สมบัติ พูลสวัด รักกันดีกับ กรกต ล้วนทร")
+สมบัติ พูลสวัด รักกันดีกับ กรกต ล้วนทร
