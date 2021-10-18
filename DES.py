6329659515fa32c30284deb7228119675754b65b
เตชสิทธ์ ล้วนทร <79820254+Techasit232000@users.noreply.github.com>
2021-10-18 19:19:50 +0700

    Add files via upload

---
+++ DES.py
@@ -0,0 +1,8 @@
+Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 08:53:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("The requested URL was rejected. Please consult with your administrator.
+
+Your support ID is: 8173481731032628953")
+       
+SyntaxError: EOL while scanning string literal
+>>> 
