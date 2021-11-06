f437fd6654da3f6d335032694a95f2da3bfdbaa8
เตชสิทธ์ ล้วนทร <79820254+Techasit232000@users.noreply.github.com>
2021-11-06 10:44:13 +0700

    Add files via upload

---
+++ เตชสิทธ์ ล้วนทร.py
@@ -0,0 +1,8 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print ("Techasit Luanthon calling Iphon that memmories this name")
+Techasit Luanthon calling Iphon that memmories this name
+print ("ราชวงศ์จักรี")
+ราชวงศ์จักรี
+print ("เตชสิทธ์ ล้วนทร")
+เตชสิทธ์ ล้วนทร
