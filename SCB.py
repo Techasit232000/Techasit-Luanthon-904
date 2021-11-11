1cea0916bdbf51984573039a0fde08797df12c19
เตชสิทธ์ ล้วนทร <79820254+Techasit232000@users.noreply.github.com>
2021-11-11 12:11:17 +0700

    Add files via upload

---
+++ SCB.py
@@ -0,0 +1,8 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print ("SCB go side down and make money for all that have SCB account")
+SCB go side down and make money for all that have SCB account
+print ("SCB Mean Siam commercial bank from The kingdom of Thailand")
+SCB Mean Siam commercial bank from The kingdom of Thailand
+print ("เตชสิทธ์ ล้วนทร Control it")
+เตชสิทธ์ ล้วนทร Control it
