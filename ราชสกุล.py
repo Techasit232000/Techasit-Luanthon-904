509b03b749a0a7a88fddeaec5959fe87a4c22c13
เตชสิทธ์ ล้วนทร <79820254+Techasit232000@users.noreply.github.com>
2021-11-15 08:58:46 +0700

    Add files via upload

---
+++ ราชสกุล.py
@@ -0,0 +1,5 @@
+Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 08:53:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("ราชสกุล,ล้วนทร,ศรีลาวงษ์,บุญหนุน,เสือสุข,ด้วงเจริญ")
+ราชสกุล,ล้วนทร,ศรีลาวงษ์,บุญหนุน,เสือสุข,ด้วงเจริญ
+>>> 
