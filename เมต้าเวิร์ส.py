5965e188f84f1bd582ec07d8c556c1f67acc206c
เตชสิทธ์ ล้วนทร <79820254+Techasit232000@users.noreply.github.com>
2021-11-16 12:23:51 +0700

    Add files via upload

---
+++ เมต้าเวิร์ส.py
@@ -0,0 +1,45 @@
+Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 08:53:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("
+<!DOCTYPE html>
+<html>
+  <head>
+<meta name="env:base_assets_path" content="https://space-prod-assets.space-metaverse.com/spoke/"/>
+<meta name="env:cors_proxy_server" content="space-prod-cors-proxy.space-metaverse.com"/>
+<meta name="env:ga_tracking_id" content=""/>
+<meta name="env:hubs_server" content="space-เมต้าเวิร์ส.com"/>
+<meta name="env:is_moz" content="false"/>
+<meta name="env:non_cors_proxy_domains" content="space-metaverse.com,space-metaverse.com,space-prod-assets.space-metaverse.com,space-prod-733845226300-hubs-worker.com"/>
+<meta name="env:reticulum_server" content="space-metaverse.com"/>
+<meta name="env:sentry_dsn" content="https://1d5750f306aa42079ae93c23e03b3cfe@o985905.ingest.sentry.io/5942344"/>
+<meta name="env:thumbnail_server" content="space-prod-nearspark.space-metaverse.com"/>
+<title> by Space AG</title>
+
+<script>window.APP_CONFIG = JSON.parse('{"translations":{"en":{"share-hashtag":"#metaverse #nfts #nft #digitalart #art #cryptoart #raredigitalart #ethereum #artoftheday #cryptoartist #blockchain #contemporaryart #animation #modernart #abstractart #crypto #bitcoin #generativeart #design #digitalillustration #nftsstories #instalike #digitaldrawing #artgallery #digitalcollectibles #stopmotion #bhfyp #sansar #secondlife #vrchat","settings.community":"Space metaverse is here! You are the people of Space. It is time now for you to explore your surroundings or start getting directly into our creator’s content creation with all the NFT, shop, concerts available to you. It is not an easy path to follow but we are in together.  Our Space community is your community.  Enjoy the ride!","contact-email":"batissamadian@mail.com","company-name":"Space AG","app-tagline":"The world like ours. Just better.","app-name":"Space","app-description":"Define who you want to be, meet the best people all around the world and build your life and career in a stunningly realistic 3D world created by anyone of us."}},"theme":{"themes":[{"variables":{},"name":"Space Default","id":"space-default","default":true},{"variables":{"primary-color":"#3332FE"},"name":"Hubs Spaces","id":"hubs-dark-mode","darkModeDefault":true}]},"links":{"terms_of_use":"https://tryspace.app/terms-conditions/","privacy_notice":"https://tryspace.app/privacy-policy/","issue_report":"https://www.tryspace.app","features":"https://discord.com/invite/TvYg2YHDyy","docs":null,"controls":"https://discord.com/invite/TvYg2YHDyy","community":"https://discord.com/invite/TvYg2YHDyy"},"images":{"logo":"https://space-prod-assets.space-metaverse.com/files/8ace9d10-f8eb-4de2-930b-33ada80890e0.png","home_background":"https://space-prod-assets.space-metaverse.com/files/f8d4c161-765b-4630-8807-e022874cbd14.png","favicon":"https://space-prod-assets.space-metaverse.com/files/864fb8a7-5ab7-4576-8e7a-7e658416cec3.png","app_thumbnail":"https://space-prod-assets.space-metaverse.com/files/ad018de1-5129-4b7a-a121-4ba8da37287d.png","app_icon":"https://space-prod-assets.space-metaverse.com/files/629cbfb7-a5e8-4ef2-a411-dd6893b0b90d.png"},"features":{"show_terms":true,"show_privacy":true,"show_issue_report_link":true,"show_docs_link":false,"show_community_link":true,"permissive_rooms":true,"max_room_size":50,"hide_powered_by":true,"enable_spoke":true,"enable_lobby_ghosts":true,"disable_sign_up":false,"default_room_size":24},"auth":{"login_subject":"Space Login","login_body":null}}')</script>
+    <!-- DO NOT REMOVE/EDIT THIS COMMENT - META_TAGS -->
+
+    <title>Scene Editor</title>
+    <link rel="shortcut icon" href="https://space-prod-assets.space-metaverse.com/spoke/assets/images/favicon.ico">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Zilla+Slab:400,700" rel="stylesheet">
+
+    <!-- Google Analytics -->
+    <!-- NOTE GA tracking is disabled by default. You'll need to set the GA_TRACKING_ID build variable to enable it. -->
+    <script>
+    if (navigator.doNotTrack !== "1") {
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    }
+    </script>
+  </head>
+  <body>
+    <div id="app"></div>
+  <script type="text/javascript" src="https://space-prod-assets.space-metaverse.com/spoke/assets/js/entry-0e348b5179c66e77f43c.js"></script></body>
+</html>
+")
+      
+SyntaxError: EOL while scanning string literal
+>>> 
