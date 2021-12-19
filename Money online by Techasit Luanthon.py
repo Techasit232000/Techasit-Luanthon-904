a6c3d8ece6b77db5803ee1371c4a734b916a9376
เตชสิทธ์ ล้วนทร <79820254+Techasit232000@users.noreply.github.com>
2021-12-19 12:43:09 +0700

    Add files via upload

---
+++ Money online by Techasit Luanthon.py
@@ -0,0 +1,6 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print "(<!doctype html><title>Blockchain.com Wallet - Exchange Cryptocurrency</title><script nonce="zbzet6JnGJgxFcONTbsgc0No0qm7pIaR">window.NONCE = 'zbzet6JnGJgxFcONTbsgc0No0qm7pIaR'</script><style nonce="zbzet6JnGJgxFcONTbsgc0No0qm7pIaR">html, body, #app, #app > div {padding: 0; margin: 0; height: 100%;} html, body {overflow: hidden;} /* hide scrollbars */ ::-webkit-scrollbar { display: none; } * { scrollbar-width: none; -ms-overflow-style: none; -webkit-font-smoothing: antialiased; }</style>
+
+<script src="/manifest.1621360505456.js"></script><script src="/vendor.96b947fc3c.js"></script><script src="/frontend.d60e8a105d.js"></script><script src="/app.9587fd0f51.js"></script>)"
+SyntaxError: unterminated string literal (detected at line 1)
